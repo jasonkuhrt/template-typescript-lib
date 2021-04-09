1d5506b4fdb0e6efca86fa73a72c3e0e7aee0d7e
Jason Kuhrt <jasonkuhrt@me.com>
2021-04-08 21:31:05 -0400

    feat: add jsdoc prettier plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 console.log('hello world')
+
+/**
+ * Does the foobar thing.
+ *
+ * @example
+ *
+ * const a = foobar({
+ *  a: 1
+ *    b: 2
+ * })
+ *
+ */
+export function foobar(): null {
+  return null
+}
