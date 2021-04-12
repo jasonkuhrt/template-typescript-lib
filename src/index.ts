3f5b1f6ac4c117a79bf0f0000627025087236c9a
Jason Kuhrt <jasonkuhrt@me.com>
2021-04-11 23:12:57 -0400

    docs: fix jsdoc example

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-console.log('hello world')
-
 /**
  * Does the foobar thing.
  *
  * @example
  *
- * const a = foobar({
- *  a: 1
- *    b: 2
- * })
+ *   const a = foobar({
+ *     a: 1,
+ *     b: 2,
+ *   })
  *
  */
-export function foobar(): null {
-  return null
-}
+export function foobar(): void {}
