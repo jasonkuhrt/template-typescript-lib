c4696110b96d0f25aef0bb11a5f381880a95d879
Jason Kuhrt <jasonkuhrt@me.com>
2024-04-10 23:46:48 -0400

    fix: eslint ignore build dir

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
 import configPrisma from 'eslint-config-prisma'
 import tsEslint from 'typescript-eslint'
 
-export default tsEslint.config(...configPrisma, {
-  ignores: [`build`, 'eslint.config.js'],
+export default tsEslint.config({
+  extends: [...configPrisma],
+  ignores: ['**/build/**/*', 'eslint.config.js'],
+  languageOptions: {
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
 })
