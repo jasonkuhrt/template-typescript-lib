777529f943ba95f278c861bbcdac78a07d91bfc5
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-25 17:47:26 +0200

    improve: test helpers scaffolding

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from 'vitest'
+import { foo } from '../tests/helpers.js'
 import * as Lib from './index.js'
 
 test(`imports using paths config works relative`, () => {
-  expect(Lib.todo()).toEqual(`nothing`)
+  expect(Lib.todo()).toEqual(foo)
 })
