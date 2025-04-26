3e3eb5b37968055e8212c80b9656968f2c681420
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-26 15:12:37 +0200

    improve: exports directory

--- src/exports/index.test.ts
+++ src/exports/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { foo } from '../tests/helpers.js'
+import { foo } from '../../tests/helpers.js'
 import * as Lib from './index.js'
 
 test(`imports using paths config works relative`, () => {
